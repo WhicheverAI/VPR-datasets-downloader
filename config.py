3e0bcee6337c7fb532dd90b21fa30ae628ad48d0
叶璨铭 <12011404@mail.sustech.edu.cn>
2024-05-03 18:39:51 +0800

    feat: privacy for tokyo dataset

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # In this config.py, you will know which urls you should fill in the config_private.py in order to makes the downloading works.
 
 
-# Email arandjelovic.relja@gmail.com to get this url.
+# Email arandjelovic.relja@gmail.com to get pitts and tokyo url.
 pitts_250k_url:str = "https://****/****/****/****/Pittsburgh250k" # Without a '/' at the end. 
+tokyo247_url:str = "https://****/****/****/****/Tokyo247"
 
 
 
